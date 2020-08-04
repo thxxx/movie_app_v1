a66d36a3ccef7960487105e482be33b4c6369d26
thxxx <zxcv05999@naver.com>
2020-08-04 11:42:45 +0900

    Updates

---
+++ precache-manifest.c4b2e29a126183636293518bf1a5386d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c52864da5f7f222df092e72dc08f42f3",
+    "url": "/movie_app_v1/index.html"
+  },
+  {
+    "revision": "12c90d3d5bb1bcc03461",
+    "url": "/movie_app_v1/static/css/main.1bfbf9c3.chunk.css"
+  },
+  {
+    "revision": "beaed01cfa40f96771f9",
+    "url": "/movie_app_v1/static/js/2.788d50ba.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_v1/static/js/2.788d50ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c90d3d5bb1bcc03461",
+    "url": "/movie_app_v1/static/js/main.cc1f9924.chunk.js"
+  },
+  {
+    "revision": "76bc87ab4666695cb825",
+    "url": "/movie_app_v1/static/js/runtime-main.c22aefa0.js"
+  }
+]);
